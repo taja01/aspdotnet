bd689028dca70ffcd9779be3dc8f8d22463c3d31
taja01 <jarvas.tamas@gmail.cim>
2025-04-29 06:17:17 +0200

    add client

---
+++ LearningAspDotNet/WeatherAppClient/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WeatherApp
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddWeatherAppClient(this IServiceCollection services, IConfiguration configuration)
+        {
+            var options = new WeatherAppOptions();
+            configuration.Bind(nameof(WeatherAppOptions), options);
+            services.Configure<WeatherAppOptions>(configuration.GetSection(nameof(WeatherAppOptions)));
+            services.AddHttpClient<IWeatherAppClient, WeatherAppClient>(c => c.BaseAddress = new Uri(options.BaseAddress));
+            return services;
+        }
+    }
+}
