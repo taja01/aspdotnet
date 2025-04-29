050cd5ddb57d51e12fe2f2bac3c024b4989c1de7
taja01 <jarvas.tamas@gmail.cim>
2025-04-29 06:17:24 +0200

    add first test

---
+++ LearningAspDotNet/WeatherAppTests/SetupFixture.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WeatherApp;
+
+namespace WeatherAppTests
+{
+    [SetUpFixture]
+    internal class SetupFixture : BaseSetUpFixture
+    {
+        public override void RegisterTestFacilities(IServiceCollection serviceCollection)
+        {
+            base.RegisterTestFacilities(serviceCollection);
+
+            serviceCollection.AddWeatherAppClient(Configuration);
+        }
+    }
+}
---
+++ LearningAspDotNet/WeatherAppTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WeatherApp;
+
+namespace WeatherAppTests
+{
+    public class Tests
+    {
+        private IWeatherAppClient _client;
+
+        [SetUp]
+        public void Setup()
+        {
+            _client = BaseSetUpFixture.ServiceProvider.GetRequiredService<IWeatherAppClient>();
+        }
+
+        [Test]
+        public async Task Test1()
+        {
+            Assert.ThrowsAsync<ApiException>(async () => await _client.GetWeatherForecastAsync().ConfigureAwait(false));
+        }
+    }
+}
